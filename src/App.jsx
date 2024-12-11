84fc41b6c14713734ba8f1562129b4b1bc286cd8
Mostary Jahan <mostaryjahan01@gmail.com>
2024-12-11 21:38:22 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+    <div>
+    <h1 className="text-2xl text-red-500 font-bold underline">
+      Hello world!
+    </h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
